eacfc43ecbaad73114e7eb1fcdda663f5469a652
Shekswess <bokicasheks@yahoo.com>
2024-04-19 15:38:28 +0200

    Added creation of datasets for Llama 3

--- src/data_processing/create_process_datasets.py
+++ src/data_processing/create_process_datasets.py
@@ -7,6 +7,7 @@ from instruct_datasets import (
     GemmaInstructDataset,
     MistralInstructDataset,
     LlamaInstructDataset,
+    Llama3InstructDataset,
 )
 
 REMOVE_COLUMNS = ["source", "focus_area"]
@@ -35,6 +36,8 @@ def process_dataset(dataset_path: str, model: str) -> pd.DataFrame:
         dataset = MistralInstructDataset(dataset_path)
     elif model == "llama":
         dataset = LlamaInstructDataset(dataset_path)
+    elif model == "llama3":
+        dataset = Llama3InstructDataset(dataset_path)
     else:
         raise ValueError(f"Model {model} not supported!")
     dataset.drop_columns(REMOVE_COLUMNS)
@@ -69,50 +72,62 @@ if __name__ == "__main__":
     mistral_datasets = []
     gemma_datasets = []
     llama_datasets = []
+    llama3_datasets = []
     for dataset_path in DATASETS_PATHS:
         dataset_name = dataset_path.split(os.sep)[-1].split(".")[0]
 
         mistral_dataset = process_dataset(dataset_path, "mistral")
         llama_dataset = process_dataset(dataset_path, "llama")
+        llama3_dataset = process_dataset(dataset_path, "llama3")
         gemma_dataset = process_dataset(dataset_path, "gemma")
 
         mistral_datasets.append(mistral_dataset)
         llama_datasets.append(llama_dataset)
+        llama3_datasets.append(llama3_dataset)
         gemma_datasets.append(gemma_dataset)
 
         mistral_dataset = create_dataset_hf(mistral_dataset)
         llama_dataset = create_dataset_hf(llama_dataset)
+        llama3_dataset = create_dataset_hf(llama3_dataset)
         gemma_dataset = create_dataset_hf(gemma_dataset)
 
-        mistral_dataset.push_to_hub(f"mistral_{dataset_name}_instruct_dataset")
-        llama_dataset.push_to_hub(f"llama_{dataset_name}_instruct_dataset")
-        gemma_dataset.push_to_hub(f"gemma_{dataset_name}_instruct_dataset")
+        # mistral_dataset.push_to_hub(f"mistral_{dataset_name}_instruct_dataset")
+        # llama_dataset.push_to_hub(f"llama2_{dataset_name}_instruct_dataset")
+        llama3_dataset.push_to_hub(f"llama3_{dataset_name}_instruct_dataset")
+        # gemma_dataset.push_to_hub(f"gemma_{dataset_name}_instruct_dataset")
 
     mistral_dataset = pd.concat(mistral_datasets, ignore_index=True)
     llama_dataset = pd.concat(llama_datasets, ignore_index=True)
+    llama3_dataset = pd.concat(llama3_datasets, ignore_index=True)
     gemma_dataset = pd.concat(gemma_datasets, ignore_index=True)
 
     mistral_dataset = create_dataset_hf(mistral_dataset)
     llama_dataset = create_dataset_hf(llama_dataset)
+    llama3_dataset = create_dataset_hf(llama3_dataset)
     gemma_dataset = create_dataset_hf(gemma_dataset)
 
-    mistral_dataset.save_to_disk(
-        os.path.join(processed_data_path, "medical_mistral_instruct_dataset")
+    # mistral_dataset.save_to_disk(
+    #     os.path.join(processed_data_path, "medical_mistral_instruct_dataset")
+    # )
+    # llama_dataset.save_to_disk(
+    #     os.path.join(processed_data_path, "medical_llama2_instruct_dataset")
+    # )
+    llama3_dataset.save_to_disk(
+        os.path.join(processed_data_path, "medical_llama3_instruct_dataset")
     )
-    llama_dataset.save_to_disk(
-        os.path.join(processed_data_path, "medical_llama_instruct_dataset")
-    )
-    gemma_dataset.save_to_disk(
-        os.path.join(processed_data_path, "medical_gemma_instruct_dataset")
-    )
+    # gemma_dataset.save_to_disk(
+    #     os.path.join(processed_data_path, "medical_gemma_instruct_dataset")
+    # )
 
-    mistral_dataset.push_to_hub("medical_mistral_instruct_dataset")
-    llama_dataset.push_to_hub("medical_llama_instruct_dataset")
-    gemma_dataset.push_to_hub("medical_gemma_instruct_dataset")
+    # mistral_dataset.push_to_hub("medical_mistral_instruct_dataset")
+    # llama_dataset.push_to_hub("medical_llama2_instruct_dataset")
+    llama3_dataset.push_to_hub("medical_llama3_instruct_dataset")
+    # gemma_dataset.push_to_hub("medical_gemma_instruct_dataset")
 
     # Smaller datasets for free colab training
     mistral_dataset_short = pd.concat(mistral_datasets, ignore_index=True)
     llama_dataset_short = pd.concat(llama_datasets, ignore_index=True)
+    llama3_dataset_short = pd.concat(llama3_datasets, ignore_index=True)
     gemma_dataset_short = pd.concat(gemma_datasets, ignore_index=True)
 
     mistral_dataset_short = pd.concat(
@@ -123,6 +138,10 @@ if __name__ == "__main__":
         [llama_dataset_short.iloc[:1000], llama_dataset_short.iloc[-5000:-4000]],
         ignore_index=True,
     )
+    llama3_dataset_short = pd.concat(
+        [llama3_dataset_short.iloc[:1000], llama3_dataset_short.iloc[-5000:-4000]],
+        ignore_index=True,
+    )
     gemma_dataset_short = pd.concat(
         [gemma_dataset_short.iloc[:1000], gemma_dataset_short.iloc[-5000:-4000]],
         ignore_index=True,
@@ -130,18 +149,23 @@ if __name__ == "__main__":
 
     mistral_dataset_short = create_dataset_hf(mistral_dataset_short)
     llama_dataset_short = create_dataset_hf(llama_dataset_short)
+    llama3_dataset_short = create_dataset_hf(llama3_dataset_short)
     gemma_dataset_short = create_dataset_hf(gemma_dataset_short)
 
-    mistral_dataset_short.save_to_disk(
-        os.path.join(processed_data_path, "medical_mistral_instruct_dataset_short")
+    # mistral_dataset_short.save_to_disk(
+    #     os.path.join(processed_data_path, "medical_mistral_instruct_dataset_short")
+    # )
+    # llama_dataset_short.save_to_disk(
+    #     os.path.join(processed_data_path, "medical_llama2_instruct_dataset_short")
+    # )
+    llama3_dataset_short.save_to_disk(
+        os.path.join(processed_data_path, "medical_llama3_instruct_dataset_short")
     )
-    llama_dataset_short.save_to_disk(
-        os.path.join(processed_data_path, "medical_llama_instruct_dataset_short")
-    )
-    gemma_dataset_short.save_to_disk(
-        os.path.join(processed_data_path, "medical_gemma_instruct_dataset_short")
-    )
+    # gemma_dataset_short.save_to_disk(
+    #     os.path.join(processed_data_path, "medical_gemma_instruct_dataset_short")
+    # )
 
-    mistral_dataset_short.push_to_hub("medical_mistral_instruct_dataset_short")
-    llama_dataset_short.push_to_hub("medical_llama_instruct_dataset_short")
-    gemma_dataset_short.push_to_hub("medical_gemma_instruct_dataset_short")
+    # mistral_dataset_short.push_to_hub("medical_mistral_instruct_dataset_short")
+    # llama_dataset_short.push_to_hub("medical_llama2_instruct_dataset_short")
+    llama3_dataset_short.push_to_hub("medical_llama3_instruct_dataset_short")
+    # gemma_dataset_short.push_to_hub("medical_gemma_instruct_dataset_short")
